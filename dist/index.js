38851d763fca9f3e22f5982f5d0d9b6dbfe58ad3
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-07-15 01:01:41 +0900

    unity-command v1.0.9

--- dist/index.js
+++ dist/index.js
@@ -3012,7 +3012,7 @@ function copyFile(srcFile, destFile, force) {
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 582:
+/***/ 531:
 /***/ ((module) => {
 
 /******/ (() => { // webpackBootstrap
@@ -3133,13 +3133,13 @@ Object.defineProperty(exports, "ArgumentBuilder", ({ enumerable: true, get: func
 
 /***/ }),
 
-/***/ 156:
+/***/ 434:
 /***/ ((__unused_webpack_module, exports, __nccwpck_require2_) => {
 
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const argument_builder_1 = __nccwpck_require2_(582);
+const argument_builder_1 = __nccwpck_require2_(531);
 class UnityCommandBuilder extends argument_builder_1.ArgumentBuilder {
     /**
      * Sets the default argument.
@@ -3351,7 +3351,7 @@ exports["default"] = UnityCommandBuilder;
 
 /***/ }),
 
-/***/ 925:
+/***/ 342:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require2_) {
 
 "use strict";
@@ -3361,15 +3361,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.UnityCommandBuilder = exports.Unity = void 0;
-var unity_1 = __nccwpck_require2_(327);
+var unity_1 = __nccwpck_require2_(887);
 Object.defineProperty(exports, "Unity", ({ enumerable: true, get: function () { return __importDefault(unity_1).default; } }));
-var UnityCommandBuilder_1 = __nccwpck_require2_(156);
+var UnityCommandBuilder_1 = __nccwpck_require2_(434);
 Object.defineProperty(exports, "UnityCommandBuilder", ({ enumerable: true, get: function () { return __importDefault(UnityCommandBuilder_1).default; } }));
 
 
 /***/ }),
 
-/***/ 327:
+/***/ 887:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require2_) {
 
 "use strict";
@@ -3400,7 +3400,7 @@ class Unity {
                     return `/Applications/Unity/Hub/Editor/${unityVersion}/Unity.app/Contents/MacOS/Unity`;
                 }
             case 'win32':
-                return `C:\\Program Files\\Unity\\Hub\\Editor\\${unityVersion}\\Editor\\Unity.exe`;
+                return `"C:\\Program Files\\Unity\\Hub\\Editor\\${unityVersion}\\Editor\\Unity.exe"`;
         }
     }
     /**
@@ -3483,7 +3483,7 @@ module.exports = __nccwpck_require__(17);
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require2_(925);
+/******/ 	var __webpack_exports__ = __nccwpck_require2_(342);
 /******/ 	module.exports = __webpack_exports__;
 /******/ 	
 /******/ })()
