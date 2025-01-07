6790ee32e2896e0e341f9b714b2d45be011c38ed
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2025-01-07 11:25:39 +0900

    fix: 自動マージでmainのプッシュトリガーが実行されないので一時的

--- publish.sh
+++ publish.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ] || [[ "$1" != "major" && "$1" != "minor" && "$1" != "patch" && "$1" != "prerelease" ]]; then
+if [ "$#" -ne 1 ] || [[ "$1" != "major" && "$1" != "minor" && "$1" != "patch" ]]; then
     echo "Usage:"
-    echo " ./publish.sh <major/minor/patch/prerelease>"
+    echo " ./publish.sh <major/minor/patch>"
     echo "  or"
     echo " npm run release <major/minor/patch>"
-    echo "  or"
-    echo " npm run pre-release"
     exit 1
 fi
 
@@ -34,21 +32,12 @@ fi
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "$1" != "prerelease" ]; then
-    if [ "$BRANCH" != "develop" ]; then
-        echo "Error: You must be on the develop branch to publish."
-        exit 1
-    fi
-
-    VERSION=$(npm version $1 --no-git-tag-version)
-else
-    if [ "$BRANCH" == "main" ] || [ "$BRANCH" == "develop" ]; then
-        echo "Error: prerlease must be outside the main or develop branch."
-        exit 1
-    fi
-
-    VERSION=$(npm version prerelease --preid rc)
+if [ "$BRANCH" != "develop" ]; then
+    echo "Error: You must be on the develop branch to publish."
+    exit 1
 fi
+
+VERSION=$(npm version $1 --no-git-tag-version)
 
 if ! git pull; then
     echo "Error: Failed to pull the latest changes."
@@ -57,7 +46,7 @@ fi
 
 git add package.json package-lock.json
 git commit -m "bump: $VERSION"
-git push --follow-tags
+git push
 
 if [ "$1" != "prerelease" ]; then
     if ! gh pr create --base main --head develop --title "bump: $VERSION" --body "bump: $VERSION"; then
